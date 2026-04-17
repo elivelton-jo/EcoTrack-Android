b41c2b2313b8f80bd2817fb8f8ae9d37fa5ba749
Elivelton <eliveltonoliveira001@gmail.com>
2026-04-17 16:40:11 -0300

    feat: adicionando tela inicial

---
+++ app/src/main/java/com/example/ecotrack/view/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.ecotrack.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.ecotrack.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Esconde a barra de título para a splash ficar limpa
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
+
+        // Delay de 2 segundos antes de ir para a MainActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 2000);
+    }
+}
